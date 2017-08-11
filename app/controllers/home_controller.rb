12bc187e843dd14845cd4c0703bf691057afa56d
sungwook <sungwook.csw@mgmail.com>
2017-08-11 17:30:33 +0900

    some layouts modified, search box view modified

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @p_rooms = Room.order(:building_id).page(params[:page]).per(15)
     @rooms = Room.all
     @departments = Department.all
     @department = Department.find(1)
