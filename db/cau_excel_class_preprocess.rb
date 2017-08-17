c20efd5ef7efd87fcd78c978db6a6f4b2532092c
sungwook <sungwook.csw@mgmail.com>
2017-08-18 03:25:16 +0900

    xlsx_class parse maded (98%)

--- db/cau_excel_class_preprocess.rb
+++ db/cau_excel_class_preprocess.rb
@@ -12,26 +12,75 @@ end
 def ltbdrm_has_room?(ltbdrm)
   return false if ltbdrm.nil?
   return false if ltbdrm.include?('재택')
-  return false unless ltbdrm.include?('호')
+  return false unless ltbdrm.include?('호') || ltbdrm.include?('<')
   return true
 end
 
 def ltbdrm_2_room(ltbdrm)
-  return nil unless ltbdrm_has_room?(ltbdrm)
+  return [nil, nil] unless ltbdrm_has_room?(ltbdrm)
+  # TODO : 강의실이 두개인 경우 : 시간에 비해 복잡성이 굉장히 상승하여 (보류)
+=begin
+  temp = ltbdrm[/^.*>/]
+  temp = temp.split('/')
+  temp_arr = []
+  if temp.class == ''.class
+    temp_arr[0] = [temp]
+  else
+    temp_arr = temp
+  end
+  temp_arr.each do |ltb|
+
+  end
+=end
   b = ltbdrm.split('관')[0].to_i
-  l = ltbdrm.split('호')[0][-3..-1].to_i
-  [b, l]
+  if b == 0
+    b = 1959 if ltbdrm.include?('공연예술원')
+    b = 801 if ltbdrm.include?('외국어문학')
+    b = 9999 if ltbdrm.include?('대 운 동 장')
+    b = 1001 if ltbdrm.include?('국악대학 가건물')
+    b = 810 if ltbdrm.include?('원형관')
+    b = 805 if ltbdrm.include?('공연영상관')
+    b = 1002 if ltbdrm.include?('국악대학')
+  end
+
+  temp = ltbdrm[/[0-9\-_]*호/]
+  if !temp.nil?
+    temp = temp.split('호')[0]
+    if temp.include?('-')
+      l = temp.split('-')[0].to_i
+      l2 = temp.split('-')[1].to_i
+    else
+      l = temp.to_i
+    end
+  else
+    possible_case = ltbdrm[/\d{4}/]
+    l = possible_case.to_i unless possible_case.nil?
+  end
+  [b, l, l2]
 end
 
 def ltbdrm_2_building_name(ltbdrm)
   return nil unless ltbdrm_has_room?(ltbdrm)
-  ra = ltbdrm.split('관')[1].split('호')[0]
-  ra[/\(.*?\)/, 1]
+
+  ra = ltbdrm.split('호')[0]
+  unless ra[/\(.*?\)/].nil?
+    return ra[/\(.*?\)/][1..-2]
+  end
+
+  ['공원예술원', '외국어문학', '대 운 동 장', '국악대학 가건물', '원형관', '공연영상관'].each do |n|
+    return n if ltbdrm.include?(n)
+  end
+
+  return nil
+
 end
 
 def ltbdrm_2_room_name(ltbdrm)
   return nil unless ltbdrm_has_room?(ltbdrm)
-  room_name = ltbdrm.split('호')[1][/<.*?>/]
+  temp = ltbdrm
+  # temp = ltbdrm.split('호')[1] if ltbdrm.include?('호')
+  room_name = temp[/<.*?>/]
+  room_name = room_name[1..-2] unless room_name.nil?
   room_name ||= '미정'
   return room_name
 end
@@ -43,7 +92,9 @@ end
 
 def ltbdrm_2_time(ltbdrm)
   return nil unless ltbdrm_has_room?(ltbdrm)
-  classes = ltbdrm.split('>')[1].split('/')
+  temp = ltbdrm[/>[a-zA-Z가-힣 0-9\/,():~]*$/]
+  classes = temp[1..-1].split('/') unless temp.nil?
+  classes ||= ltbdrm.split('>')[1].split('/')
 
   # /를 통해 요일별로 나눈것을 array화 한다
   classes_arr = []
@@ -66,25 +117,26 @@ def ltbdrm_2_time(ltbdrm)
     end
     next if yoyil == '없음'
 
-    time_str = [(str.index(yoyil) + 1)..-1]
+    time_str = str[(str.index(yoyil) + 1)..-1]
+    # puts "time_str = #{time_str}" ##
     if time_str.include?('~')
-      wave = str.index('~')
-      st = str[(wave-5)..(wave-4)].to_i * 2
-      st += str[(wave-2)..(wave-1)].to_i > 0 ? 1 : 0
-      fi = str[(wave+1)..(wave+2)].to_i * 2
-      fi += str[(wave+3)..(wave+5)].to_i > 0 ? 1 : 0
-      fi += 1
+      wave = time_str.index('~')
+      st = time_str[(wave-2)..(wave-1)].to_i
+      st += time_str[(wave-5)..(wave-4)].to_i * 100
+
+      fi = time_str[(wave+4)..(wave+5)].to_i
+      fi += time_str[(wave+1)..(wave+2)].to_i * 100
 
     else
-      nums = str[(str.index(yoyil)+1)..-1]
+      nums = time_str
       nums = nums.split(',')
       nums = nums.collect{ |s| s.to_i }
       nums = nums.sort
 
-      st = lut(nums[0]) * 2
-      fi = lut(nums[-1] + 1) * 2
+      st = lut(nums[0]) * 100
+      fi = lut(nums[-1] + 1) * 100
     end
-    time_info = {week: yoyil, st: st, fi: fi}
+    time_info = { week: yoyil, st: st, fi: fi }
     time_class.push(time_info)
   end
 
@@ -115,13 +167,14 @@ profnm = sheet.column(12) # 서의석
 ltbdrm = sheet.column(13) # 302(대학원) 501 <강의실>
 puts 'done'
 
-list = %w(과목명 교수 캠퍼스 과목명 분반 년도 학기 분류 전공1 전공2 강의실여부 건물이름 건물 호수 강의실이름 강의들)
+list = %w(과목명 교수 캠퍼스 과목명 분반 년도 학기 분류 전공1 전공2 강의실여부 건물이름 건물 호수1 호수2 강의실이름 강의들)
 list.each_with_index do |l, i|
   worksheet.write(0, i, l)
 end
 
 (1...year.size).each do |i|
-  puts i.to_s
+  # puts i.to_s.rjust(4, '0') ##
+  # puts ltbdrm[i] ##
   j = 0
   one_list = [kornm[i], profnm[i], campcd[i],
               sbjtno1[i], clssno1[i],
@@ -130,17 +183,27 @@ end
   ]
   colgnm_2_major(colgnm[i]).each{ |m| one_list.push(m) }
   one_list.push(ltbdrm_has_room?(ltbdrm[i]))
-  next unless ltbdrm_has_room?(ltbdrm[i])
   one_list.push(ltbdrm_2_building_name(ltbdrm[i]))
   ltbdrm_2_room(ltbdrm[i]).each{ |n| one_list.push(n) }
   one_list.push(ltbdrm_2_room_name(ltbdrm[i]))
-  one_list.push(ltbdrm_2_time(ltbdrm[i]))
 
-  binding.pry
+  times = ltbdrm_2_time(ltbdrm[i])
+  unless times.nil?
+    times.each do |t|
+      temp_str = ''
+      t.each { |k, v| temp_str += v.to_s + ' ' }
+      one_list.push(temp_str)
+    end
+  end
+
+  # print one_list ##
+  # puts '' ##
+  # binding.pry
+
   one_list.each do |n|
     c = n.to_s
     c ||= '0'
-    worksheet.write(i + 1, j, c)
+    worksheet.write(i, j, c)
     j += 1
   end
 end
